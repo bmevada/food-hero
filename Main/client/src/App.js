9fdf8ae49336d064b1d5ecdffa549d213cf6e064
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2023-02-04 19:01:40 +1100

    review and update index css and js

--- Main/client/src/App.js
+++ Main/client/src/App.js
@@ -1,42 +1,22 @@
+import './App.scss';
 import React from 'react';
+import Routing from './routes';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+import Layout from './layout'
+import axios from 'axios'
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+axios.defaults.baseURL="http://localhost:4000"
 
 function App() {
+ 
   return (
-    <ApolloProvider client={client}>
+    <div className="App">
       <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />}
-            />
-            <Route 
-              path="/matchup" 
-              element={<Matchup />}
-            />
-            <Route 
-              path="/matchup/:id" 
-              element={<Vote />}
-            />
-            <Route 
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
-        </div>
+        <Layout>
+          <Routing />
+        </Layout>
       </Router>
-    </ApolloProvider>
+    </div>  
   );
 }
 
--- Main/client/src/index.js
+++ Main/client/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../src/redux/reducer/index'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENDION__&&
+  window.__REDUX_DEVTOOLS_EXTENDION__()
+)
+
+render (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>, document.getElementById('root')
+)
+
+reportWebVitals();
