35d123a0ffce4c2d60223f5e89a3d41adad57805
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2023-02-08 18:48:03 +1100

    review of checkout form and styles added

---
+++ Main/client/src/pages/CheckOut/CheckOutForm/index.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { useDispatch } from "react-redux"
+import Button from '../../../components/Button/button1';
+import { loadStripe } from "@stripe/stripe-js"
+import {
+  Elements,
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
+} from "@stripe/react-stripe-js"
+import './style.scss'
+import { useNavigate } from 'react-router';
+import { addToCart } from "../../../redux/actions";
+import axios from 'axios';
+
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PK}`)
+
+const PaymentForm = (props) => {
+  const [holder, setHolder] = useState("")
+  const elements = useElements()
+  const stripe = useStripe()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      if (!stripe || !elements) return
+      const cardNumberElement = elements?.getElement(CardNumberElement)
+      if (!cardNumberElement) console.log('No exit card!')
+      const token = await stripe.createToken(cardNumberElement, { name: holder })
+      if (token.error) console.log(token.error.message)
+      axios.post('/orders', {
+        order: props.order,
+        token: token.token
+      }, {
+        headers: {
+          'x-auth-token': `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        }
+      }).then(res => res.data)
+        .then(data => {
+          if(data.success) {
+            dispatch(addToCart([]))
+            localStorage.removeItem("cart")
+            navigate('/')
+          }
+        }).catch(err => console.log(err))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    const cardNumberElement = elements?.getElement(CardNumberElement)
+    if (cardNumberElement) cardNumberElement.clear()
+    const expiryElement = elements?.getElement(CardExpiryElement)
+    if (expiryElement) expiryElement.clear()
+    const cvcElement = elements?.getElement(CardCvcElement)
+    if (cvcElement) cvcElement.clear()
+    setHolder("")
+  }, [props.show])
+
+  const elementStyle = {
+    lineHeight: '42px',
+    color: '#54504E'
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="card-types">
+        <div className="letter">
+          <span>Please Enter card Details</span>
+        </div>
+      </div >
+      <div className="card-number">
+        <CardNumberElement
+          options={{
+            style: {
+              base: elementStyle,
+            },
+          }}
+        />
+      </div>
+      <div className="card-holder-name">
+        <div className="letter">
+          <span>Card Holder</span>
+        </div>
+        <div className="holder-name">
+          <input
+            value={holder}
+            onChange={(e) => setHolder(e.target.value)}
+            placeholder="CardHolder Name"
+          />
+        </div>
+      </div>
+      <div className="expire-cvv">
+        <div className="expire">
+          <div className="letter">
+            <span>Expiry</span>
+          </div>
+          <div className="expire-ele">
+            <CardExpiryElement
+              options={{
+                style: {
+                  base: elementStyle,
+                },
+              }}
+            />
+          </div>
+        </div>
+        <div className="cvv">
+          <div className="letter">
+            <span>CVC</span>
+          </div>
+          <div className="cvv-ele">
+            <CardCvcElement
+              options={{
+                style: {
+                  base: elementStyle,
+                },
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="pay-btn" style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        <Button value="Pay" width="150px" onClick={() => { }} />
+      </div>
+    </form>
+  )
+}
+
+const CheckOutModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">CheckOut</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Elements stripe={stripePromise}>
+          <PaymentForm {...props} />
+        </Elements>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default CheckOutModal;
---
+++ Main/client/src/pages/CheckOut/index.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react'
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import InputAdornment from '@mui/material/InputAdornment';
+import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { VscTriangleDown, VscTriangleUp } from "react-icons/vsc";
+import { BsDashCircle } from "react-icons/bs"
+import { addToCart } from '../../redux/actions';
+import Button from "../../components/Button/button1"
+import CheckOutModal from './CheckOutForm';
+import "./style.scss"
+
+const CheckOut = () => {
+  const dispatch = useDispatch()
+  const items = useSelector(state => state.items);
+
+  const [active, setActive] = useState(false)
+  const [address, setAddress] = useState("")
+  const [phone, setPhone] = useState("")
+  const [extra, setExra] = useState("")
+  const priceSubTotal = items.reduce((accumulator, value) => { return accumulator + value.price * value.count; }, 0)
+
+  const remove = (index) => {
+    const products = items.filter((item, i) => i !== index)
+    localStorage.setItem("cart", JSON.stringify(products))
+    dispatch(addToCart(products))
+  }
+
+  const InputCSS = {
+    '&.MuiInputBase-root:after': { borderBottom: '2px solid black' },
+    ':hover:not(.Mui-disabled):before': { borderBottom: '2px solid rgba(0, 0, 0, 0.6)' },
+    '&.MuiInputBase-root:before': { borderBottom: '1px solid rgba(0, 0, 0, 0.6)' },
+  }
+  const LabelCSS = {
+    '&.Mui-focused': { color: 'rgba(0, 0, 0, 0.6)' }
+  }
+  const InputOtherCSS = {
+    width: '100%',
+    marginBottom: '20px',
+    ':before': { borderBottom: '1px solid rgba(0, 0, 0, 0.6)' },
+    ':hover:not(.Mui-disabled):before': { borderBottom: '2px solid rgba(0, 0, 0, 0.6)' },
+    ':after': { borderBottom: '2px solid black' }
+  }
+  const FormCSS = { width: '80%' }
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='checkOut-container'>
+      <div className='main-title'>CHECK OUT</div>
+      <div className='main-content'>
+        <div className='summary'>
+          <div className='summary-title'>Order Summary</div>
+          <hr />
+          <div className='detail-group'>
+            <div className='summary-detail'>
+              <div className='detail-title'>Total Price:</div>
+              <div className='detail-price'>{"$ " + (priceSubTotal).toFixed(2)}</div>
+            </div>
+            <div className='summary-detail'>
+              <div className='detail-title'>Stripe Fee:</div>
+              <div className='detail-price'>{"$ " + (priceSubTotal * 0.0034 + 0.5).toFixed(2)}</div>
+            </div>
+          </div>
+          <hr />
+          <div className='total'>
+            <div className='total-title'>Total:</div>
+            <div className='total-price'>{"$ " + (priceSubTotal * 1.0034 + 0.5).toFixed(2)}</div>
+          </div>
+          <Button value="Place Order" onClick={() => setIsOpen(true)} />
+        </div>
+        <div className='review'>
+          <div className='review-order'>
+            <div className='order-title' onClick={() => setActive(!active)}>
+              <div className='title-txt'>1. Review Order</div>
+              <div className='title-symbol'>
+                {active ? <VscTriangleDown /> : <VscTriangleUp />}
+              </div>
+            </div>
+            <hr />
+            {active &&
+              <div className='order-content'>
+                {items.map((item, index) => (
+                  <div className='review-individual' key={index}>
+                    <div className='individual-img'>
+                      <img src={`${process.env.REACT_APP_SERVER_URL}/${item.img}`} alt="Review" />
+                    </div>
+                    <div className='individual-content'>
+                      <div className='individual-desc'>
+                        <div className="combo-part">
+                          <div className="combo-content">
+                            <span className="key">Name: </span>
+                            <span className="value">{item.title}</span>
+                          </div>
+                          <div className="combo-content">
+                            <span className="key">Price: </span>
+                            <span className="value">$ {item.price}</span>
+                          </div>
+                          <div className="combo-content">
+                            <span className="key">Quantity: </span>
+                            <span className="value">{item.count}</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='individual-final'>
+                        <div className='remove-button' onClick={() => remove(index)}><BsDashCircle style={{ fontSize: "12px" }} /> Remove</div>
+                        <div className='individual-price'>{"$ " + (item.count * item.price).toFixed(2)}</div>
+                      </div>
+                    </div>
+                  </div>
+                )
+                )}
+                <hr />
+              </div>
+            }
+          </div>
+          <div className='address'>
+            <div className='address-title'>
+              <div className='title-txt'>2. Address</div>
+            </div>
+            <Input
+              value={address}
+              fullWidth
+              onChange={e => setAddress(e.target.value)}
+              id="formatted-text-mask-input"
+              sx={{ ...InputCSS }}
+            />
+            <div className='contact-info'>
+              <div className='contact-title'>Contact Information</div>
+              <div className='contact-option'>
+                <div className='input-info'>
+                  <FormControl variant="standard" sx={{ ...FormCSS }}>
+                    <InputLabel htmlFor="formatted-text-mask-input" sx={{ ...LabelCSS }}>Phone Number</InputLabel>
+                    <Input
+                      value={phone}
+                      placeholder="+61 222 222 222"
+                      onChange={e => setPhone(e.target.value)}
+                      name="textmask"
+                      id="formatted-text-mask-input"
+                      sx={{ ...InputCSS }}
+                    />
+                  </FormControl>
+                  <FormControl variant="standard" sx={{ ...FormCSS }}>
+                    <InputLabel htmlFor="input-with-icon-adornment" sx={{ ...LabelCSS }}>
+                      Extra(Optional)
+                    </InputLabel>
+                    <Input
+                      id="input-with-icon-adornment"
+                      value={extra}
+                      onChange={e => setExra(e.target.value)}
+                      startAdornment={
+                        <InputAdornment position="start"  >
+                          <AccountCircle />
+                        </InputAdornment>
+                      } sx={{ ...InputOtherCSS }}
+                    />
+                  </FormControl>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <CheckOutModal
+        show={modalIsOpen}
+        order={{
+          totalPrice: priceSubTotal * 1.0034 + 0.5,
+          orders: items,
+          address: address,
+          phone: phone,
+          extra: extra,
+        }}
+        onHide={() => setIsOpen(false)}
+      />
+    </div>
+  )
+}
+
+export default CheckOut;
