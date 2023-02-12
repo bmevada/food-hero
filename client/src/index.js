26c7cd120fcec41ca335aed5647d3374109bbb53
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2023-02-13 10:59:53 +1100

    debugging

--- client/src/index.js
+++ client/src/index.js
@@ -1,46 +1,31 @@
-// import React, { Component } from 'react';
-// import Header from './layout/Header';
-// import Footer from './layout/Footer';
-// import Cart from './layout/Cart/cart';
-
-// class Layout extends Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-
-//         }
-//     }
-
-//     render() {
-//         return <div className='layout'>
-//             <Header />
-//             <div className='content'>
-//                 {this.props.children}
-//             </div>
-//             <Cart />
-//             <Footer />
-
-//         </div>;
-//     }
-// }
-
-// export default Layout;
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../src/redux/reducer/index'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENDION__&&
+  window.__REDUX_DEVTOOLS_EXTENDION__()
+)
+
+render (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>, document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+reportWebVitals();
