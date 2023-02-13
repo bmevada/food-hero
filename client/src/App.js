3bca77fa2e558e6229c606a3ec4b7ce8719886dd
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2023-02-13 11:25:52 +1100

    issues fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import './App.scss';
+// import './App.scss';
+// import React from 'react';
+// import Routing from './routes';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Layout from '.'
+// import axios from 'axios'
+
+// axios.defaults.baseURL = "http://localhost:4000"
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Layout>
+//           <Routing />
+//         </Layout>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
-import Routing from './routes';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from '.'
-import axios from 'axios'
+// import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-axios.defaults.baseURL = "http://localhost:4000"
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-function App() {
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../src/redux/reducer/index'
 
-  return (
-    <div className="App">
-      <Router>
-        <Layout>
-          <Routing />
-        </Layout>
-      </Router>
-    </div>
-  );
-}
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENDION__&&
+  window.__REDUX_DEVTOOLS_EXTENDION__()
+)
+
+render (
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+)
+
+reportWebVitals();
 
 export default App;
-
-
